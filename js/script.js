17daf2fab89772dae0082bba842f41c818bcca10
Margub Murshed <murshedmargub@gmail.com>
2025-01-25 15:28:22 +0600

    added functions for generating random alphabet and showing it to the screen

--- js/script.js
+++ js/script.js
@@ -1,4 +1,16 @@
+function continueGame(){
+    // generating random alphabet
+    const alphabet = generateRandomAlphabet();
+
+    // setting the random alphabet to the screen
+    setElementValueById('random-alphabet-show', alphabet)
+
+    //setting background color to the matched key
+    addClassToElementById(alphabet, 'bg-orange-500')
+}
+
 function play(){
     hideElementById('home-section');
     showElementById('playground-section');
+    continueGame();
 }
--- js/utility.js
+++ js/utility.js
@@ -7,3 +7,20 @@ function showElementById(elementId){
     const element = document.getElementById(elementId);
     element.classList.remove('hidden');
 }
+
+function generateRandomAlphabet(){
+    const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
+    const alphabetsArray = alphabetString.split("");
+    const randomNumber = Math.round(Math.random()*25);
+    const randomAlphabet = alphabetsArray[randomNumber];
+    return randomAlphabet;
+}
+
+function setElementValueById(elementId, value){
+    const element = document.getElementById(elementId);
+    element.innerText = value
+}
+function addClassToElementById(elementId, className){
+    const element = document.getElementById(elementId);
+    element.classList.add(className);
+}
